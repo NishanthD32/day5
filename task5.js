7fd353833e4c1895e88a9827d6314d59429c6851
NishanthD32 <nishanth323232@gmail.com>
2023-05-18 15:40:10 +0530

    day5

---
+++ task5.js
@@ -0,0 +1,106 @@
+//for loop
+// var json = [{
+//     "key1": "value1",
+//     "key2": "value2",
+//     "key3": "value3"
+//   },
+//   {
+//     "key4": "value4",
+//     "key5": "value5",
+//     "key6": "value6"
+//   }
+//   ];
+//   for (var i=0;i<json.length;i++) {
+//       console.log(json[i]);
+//     }
+  //for in loop
+//   let car =
+//     {
+//         name : "kia",
+//         model: "xxll",
+//         no :  1234,
+//         price:{
+//             model1 : 120,
+//             model2 :140,
+//         }
+
+//     }
+  
+//     for (let key in car){
+//         console.log(key,car[key])
+//     }
+//for of loop
+// let car =[
+//     {
+//         name : "kia",
+//         model: "xxll",
+//         no :  1234,
+//         price:{
+//             model1 : 120,
+//             model2 :140,
+//         }
+
+//     }
+//     ]
+  
+//     for (let x of car){
+//         console.log(x)
+//     }
+
+//forEach loop
+//   let car =[
+//     {
+//         name : "kia",
+//         model: "xxll",
+//         no :  1234,
+//         price:{
+//             model1 : 120,
+//             model2 :140,
+//         }
+
+//     }
+//     ]
+//     car.forEach(function(index) {
+//     console.log(index);
+// });
+
+// Json Resume
+
+// let resume=[
+//   `{
+// personaldetails {
+//   name : "Nishanth D",
+//   phone : 1234567890,
+//   email : "guvi@gmail.com",
+//   address{
+//     no : 61, village : "cuddalore", state : "tamilnadu", pincode : "123456"
+//   }
+// },
+// education{
+//   sslc{
+//     school : "government hr school",
+//     year : 2014 - 2016,
+//     percentage : 70%
+//   },
+//   hsc{
+//     school : "government hr school",
+//     year : 2016 - 2018,
+//     percentage : 75%
+//   },
+//   college{
+//     college : "a2d university",
+//     year : 2018 - 2021,
+//     percentage : 65%
+//   }
+// },
+// biodata{
+// name : "Nishanth D",
+// fathername : "durairaj",
+// language : "tamil",
+// address {
+//   no : 61, village : "cuddalore", state : "tamilnadu" pincode : "123456"
+// }
+
+// }
+// }`];
+// console.log(resume);
